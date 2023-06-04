0c60ab6e2521f95f0ef78f0418b926d36cf77ad8
Alyssa Altimas <ataltimas@gmail.com>
2023-06-03 17:07:00 -0700

    Initial Commit

---
+++ ImagePicker/ImagePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickerApp.swift
+//  ImagePicker
+//
+//  Created by scholar on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
